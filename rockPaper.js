88d3004d9c0475249268cd582c2e6149b6ae7a6e
egamIrorriM <31594132+egamIrorriM@users.noreply.github.com>
2019-04-24 23:30:37 -0700

    Add files via upload

---
+++ rockPaper.js
@@ -0,0 +1,529 @@
+/*
+Matthew Chambers
+CSC 337, Spring 2019
+Homework 11
+
+holds all of the logic fore the rockPaper html
+sends messages to rockPaper_service.js and holds
+log ins.
+uses cookies to keep track of log ins if the buttons is clicked.
+*/
+
+
+"use strict";
+
+(function() {
+	
+	//http://localhost:3000
+	//http://ec2-52-206-46-141.compute-1.amazonaws.com/:3000
+	let SERVER_ADDRESS = "http://ec2-3-82-125-47.compute-1.amazonaws.com/:3000";
+	let loggedInUserName = null;
+	let buttonClicks = 0;
+	
+	window.onload = function() {
+		
+		//enterLogIn();
+		//console.log (document.cookie);
+		let sessionId = getSessionIDfromCookie();
+		if ( sessionId < 0){
+			enterLogIn();
+	
+		} else {
+			
+			checkSessionID( sessionId);
+			
+			
+		}
+		
+	};
+	
+	
+	/** returns the session id stored in the cookie*/
+	function getSessionIDfromCookie(){
+		let id = getCookie("sessionID");
+		if(id == null){
+			return -1;
+		}
+		
+		let sessionId = parseInt(id);
+		
+		return sessionId;
+		
+	}
+	
+	
+	/** checks to see if the session id is valid*/
+	function checkSessionID(sessionId){
+		
+		const fetchOptions = makeFetchOptions( "checkSession", sessionId, "", "", 0);
+		
+		let url = SERVER_ADDRESS;
+		
+		fetch(url, fetchOptions)
+			.then(checkStatus)
+			.then(function(responseText) {
+				let json = JSON.parse(responseText);
+				handleSessionResponse(json);
+			
+			})
+			.catch(function(error) {
+				let logErrorDiv = document.getElementById("errorLog");
+				logErrorDiv.innerHTML = error;
+				
+			});
+		
+		
+	}
+	
+	
+	/** takes care of the results of the sesion id check*/
+	function handleSessionResponse(json){
+		
+		if(json["result"] == "success") {
+					
+			let logErrorDiv = document.getElementById("errorLog");
+			logErrorDiv.innerHTML =  "";
+					 
+			loggedInUserName = json["userName"];
+			buttonClicks = json["clicks"];
+			
+			enterLobby();
+					
+		} else {
+			
+			enterLogIn();
+		}
+		
+	}
+	
+	/** checks that the paramaters are of valid size*/
+	function checkParamSize( param, paramName){
+		if(param.length < 3 || param.length > 16 ){
+			let logErrorDiv = document.getElementById("errorLog");
+			logErrorDiv.innerHTML =  paramName + " must be between 3 and 16 characters long.";
+			return -1;
+		}
+		
+		return 0;
+		
+	}
+	
+	
+	
+	/** sends a request to service to try to log In
+	*/
+	function logInAttempt() {
+	
+		let username = document.getElementById("loginName").value;
+		let pass = document.getElementById("loginPass").value;
+		
+		
+		if( checkParamSize(username, "Username") < 0 || checkParamSize(pass, "Password") < 0){
+			return;
+		}
+		
+		const fetchOptions = makeFetchOptions( "logIn", "", username, pass, 0);
+		
+		let url = SERVER_ADDRESS;
+		
+		fetch(url, fetchOptions)
+			.then(checkStatus)
+			.then(function(responseText) {
+				let json = JSON.parse(responseText);
+				handleLogInResponse(json);
+			
+			})
+			.catch(function(error) {
+				let logErrorDiv = document.getElementById("errorLog");
+				logErrorDiv.innerHTML = error;
+				
+			});
+		
+	}
+	
+	
+	/** creates the message, and fetch options object to send to service*/
+	function makeFetchOptions( requestType, sessionId, username, passw, click) {
+		
+		const message = {request: requestType,
+					session: sessionId,
+					userName: username, 
+					pass: passw,
+					clicks: click};
+	
+		const fetchOptions = {
+			method : 'POST',
+			headers : {
+				'Accept': 'application/json',
+				'Content-Type' : 'application/json'
+			},
+			body : JSON.stringify(message)
+		};
+		
+		return fetchOptions;
+	}
+	
+	
+	/** handles the log in response from server*/
+	function handleLogInResponse( json) {
+		
+		if(json["result"] == "success") {
+					
+			let logErrorDiv = document.getElementById("errorLog");
+			logErrorDiv.innerHTML =  "";
+					 
+			loggedInUserName = json["userName"];
+			buttonClicks = json["clicks"];
+			
+			makeCookie( "sessionID", json["session"]);
+			
+			enterLobby();
+					
+		} else {
+			let logErrorDiv = document.getElementById("errorLog");
+			logErrorDiv.innerHTML =  "Username or password not recognized.";
+					
+		}
+		
+	}
+	
+	
+	
+	/** determins if the new user input set is a viable set
+	*/
+	function checkNewUserValidity(username, pass, passRep){
+		
+		if( checkParamSize(username, "Username") < 0 || checkParamSize(pass, "Password") < 0){
+			return;
+		}
+		
+		if(pass != passRep){
+			let logErrorDiv = document.getElementById("errorLog");
+			logErrorDiv.innerHTML =  "passwords dont match.";
+			return 0;
+		}
+		
+		return -1;
+		
+	}
+	
+	/** sends a request to rockPaper_service to add this 
+		name password pair to be a new user account.
+	*/
+	function makeNewUser() {
+		let username = document.getElementById("newName").value;
+		let pass = document.getElementById("newPass").value;
+		let passRep = document.getElementById("newPassRep").value;
+		
+		if( checkNewUserValidity(username, pass, passRep) == 0) {
+			return;
+		}
+		
+		const fetchOptions = makeFetchOptions("addUser", "", username, pass, 0);
+		
+		let url = SERVER_ADDRESS;
+		
+		fetch(url, fetchOptions)
+			.then(checkStatus)
+			.then(function(responseText) {
+				let logErrorDiv = document.getElementById("errorLog");
+				logErrorDiv.innerHTML = responseText;
+			
+			})
+			.catch(function(error) {
+				let logErrorDiv = document.getElementById("errorLog");
+				logErrorDiv.innerHTML = error;
+				
+			});
+	
+		
+	}
+	
+	
+	/** sends a request to the server save the current number of clicks to the database*/
+	function saveClicks() {
+		
+		let sessionId = getSessionIDfromCookie();
+		
+		const fetchOptions = makeFetchOptions("updateClicks", sessionId, "", "", buttonClicks);
+		
+		let url = SERVER_ADDRESS;
+		
+		fetch(url, fetchOptions)
+			.then(checkStatus)
+			.then(function(responseText) {
+				let json = JSON.parse(responseText);
+				handleClicksResponse(json);
+				
+				
+			})
+			.catch(function(error) {
+				let logErrorDiv = document.getElementById("errorLog");
+				logErrorDiv.innerHTML = error;
+				
+			});
+		
+	}
+	
+	
+	/** handles the json response from server for saving cliccks*/
+	function handleClicksResponse(json){
+		
+		if(json["result"] == "success") {
+			let logErrorDiv = document.getElementById("errorLog");
+			logErrorDiv.innerHTML =  "Save successfull.";	
+					
+		} else {
+			let logErrorDiv = document.getElementById("errorLog");
+			logErrorDiv.innerHTML =  "Error occured unable to save clicks.";
+					
+		}
+		
+	}
+	
+
+	
+	/** displays all of the lobby buttons and text divs to the HTML*/
+	function enterLobby(){
+		
+		clearPage();
+		
+		addButtonSection();
+		
+	}
+	
+	
+
+	
+	/** creates the simplest of cookies*/
+	function makeCookie( name, value) {
+		document.cookie = name + "=" + value;
+	}
+	
+	/** creates a cookie that holds the username and has an experation date*/
+	function makeExpireCookie() {
+		 //document.cookie = ID_COOKIE_NAME + "=" + loggedInUserName;
+		 
+		let name = "rockPaperId";
+		let value = loggedInUserName;
+		let expires;
+		let days = 1;
+		
+        let date = new Date();
+        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+        expires = ", expires=" + date.toGMTString();
+  
+		document.cookie = name + "=" + value + expires;
+		
+	}
+	
+	
+	/** this code is taken from https://www.w3schools.com/js/js_cookies.asp */
+	function getCookie(cname) {
+		let name = cname + "=";
+		let decodedCookie = decodeURIComponent(document.cookie);
+		let ca = decodedCookie.split(';');
+		for(let i = 0; i <ca.length; i++) {
+			let c = ca[i];
+			while (c.charAt(0) == ' ') {
+			  c = c.substring(1);
+			}
+			if (c.indexOf(name) == 0) {
+			  return c.substring(name.length, c.length);
+			}
+		  }
+		  return "";
+	}
+
+	
+	
+	
+	
+	
+	
+	
+	
+	/** logs the user out of the site and displays the log in screen*/
+	function logOut() {
+		
+		makeCookie("sessionID", "-1");
+		loggedInUserName = null;
+		
+		enterLogIn();
+	}
+	
+	/** adds everything to the HTML to display the log in screen*/
+	function enterLogIn() {
+		clearPage();
+		
+		addLogInDiv();
+		
+		addNewUserDiv();
+		
+	}
+	
+	/** adds the HTML objects to allow a user to log in*/
+	function addLogInDiv(){
+		let logInSec = document.getElementById("logIn");
+		
+		let userTitle = document.createElement("h2");
+		userTitle.innerHTML = "Log In";
+		logInSec.appendChild(userTitle);
+		
+		let userName = document.createElement("h3");
+		userName.innerHTML = "Username:";
+		logInSec.appendChild(userName);
+		
+		let nameText = document.createElement("textarea");
+		nameText.id = "loginName";
+		nameText.rows = 1;
+		nameText.cols = 30;
+		logInSec.appendChild(nameText);
+		
+		let userPass = document.createElement("h3");
+		userPass.innerHTML = "Password:";
+		logInSec.appendChild(userPass);
+		
+		let namePass = document.createElement("textarea");
+		namePass.id = "loginPass";
+		namePass.rows = 1;
+		namePass.cols = 30;
+		logInSec.appendChild(namePass);
+		
+		let logButton = makeNewButton( "logInButton", "Log In", logInSec);
+		logButton.onclick = logInAttempt;
+	
+		
+	}
+	
+	
+	/** adds the HTML objects to rockPaper.html to allow the 
+		user to make a new account
+	*/
+	function addNewUserDiv(){
+		let newUserSec = document.getElementById("newUser");
+		
+		let userTitle = document.createElement("h2");
+		userTitle.innerHTML = "New user";
+		newUserSec.appendChild(userTitle);
+		
+		let userName = document.createElement("h3");
+		userName.innerHTML = "Username:";
+		newUserSec.appendChild(userName);
+		
+		let nameText = document.createElement("textarea");
+		nameText.id = "newName";
+		nameText.rows = 1;
+		nameText.cols = 30;
+		newUserSec.appendChild(nameText);
+		
+		let userPass = document.createElement("h3");
+		userPass.innerHTML = "Password:";
+		newUserSec.appendChild(userPass);
+		
+		let namePass = document.createElement("textarea");
+		namePass.id = "newPass";
+		namePass.rows = 1;
+		namePass.cols = 30;
+		newUserSec.appendChild(namePass);
+		
+		let userPassRep = document.createElement("h3");
+		userPassRep.innerHTML = "Repeat Password:";
+		newUserSec.appendChild(userPassRep);
+		
+		let namePassRep = document.createElement("textarea");
+		namePassRep.id = "newPassRep";
+		namePassRep.rows = 1;
+		namePassRep.cols = 30;
+		newUserSec.appendChild(namePassRep);
+		
+		
+		let logButton = makeNewButton( "newUserButton", "Make new user", newUserSec);
+		logButton.onclick = makeNewUser;
+		
+		
+	}
+	
+			/** adds the various options buttons to the HTML
+		mainly used for the cookie example
+	*/
+	function addButtonSection(){
+		
+		let buttonDiv = document.getElementById("buttonDiv");
+		
+		let basicCookieButton = makeNewButton( "basicCookieButton", "Click Me", buttonDiv);
+		basicCookieButton.onclick = increaseClicks;
+		
+		
+		let clicksHeader = document.createElement("h3");
+		clicksHeader.innerHTML = "Life time clicks";
+		buttonDiv.appendChild(clicksHeader);
+		
+		let totalClicks = document.createElement("h2");
+		totalClicks.id = "totalClicks";
+		totalClicks.innerHTML = buttonClicks;
+		buttonDiv.appendChild(totalClicks);
+		
+		
+		let saveButton = makeNewButton( "saveButton", "save", buttonDiv);
+		saveButton.onclick = saveClicks;
+		
+		
+		let logoutButton = makeNewButton( "logoutButton", "Log Out", buttonDiv);
+		logoutButton.onclick = logOut;
+		
+	}
+	
+	/** increases clicks by 1*/
+	function increaseClicks(){
+		buttonClicks++;
+		
+		let totalClicks = document.getElementById("totalClicks");
+		totalClicks.innerHTML = buttonClicks;
+	}
+	
+	
+	
+	
+	/** removes all of the HTML objects that have been created by rockPaper.js*/
+	function clearPage(){
+		document.getElementById("logIn").innerHTML = "";
+		
+		document.getElementById("newUser").innerHTML = "";
+		
+		document.getElementById("errorLog").innerHTML = "";
+		
+		document.getElementById("buttonDiv").innerHTML = "";
+		
+		
+	}
+	
+	/** creates a new HTML button object with the given id, text on the button
+		and attaches it to the 'attachTo' object
+	*/
+	function makeNewButton(newId, innerText, attachTo){
+		
+		let newButton = document.createElement("button");
+		newButton.id = newId;
+		newButton.innerHTML = innerText;
+		attachTo.appendChild(newButton);
+		return newButton;
+		
+	}
+	
+	
+	/** checks the status of the return AJAX result, returns an error if needed*/
+	function checkStatus(response) {  
+		if (response.status >= 200 && response.status < 300) {  
+			return response.text();
+		} else if (response.status == 410) {
+			return Promise.reject(new Error("There is no data on this state.")); 
+		} else {  
+			return Promise.reject(new Error(response.status+": "+response.statusText)); 
+		} 
+	}
+	
+
+
+	
+}) ();
---
+++ rockPaper_service.js
@@ -0,0 +1,296 @@
+/*
+Matthew Chambers
+CSC 337, Spring 2019
+Homework 11
+
+This js acts as a middle man to allow the rockPaper.js
+to communicate with the database
+requires the following to be installed to run:
+	node
+	mysql
+	
+*/
+
+"use strict";
+
+(function() {
+		
+	const express = require("express");
+	const app = express();
+	let fs = require('fs');
+	let mysql = require('mysql');
+	
+	const  bodyParser = require('body-parser');
+	const jsonParser = bodyParser.json();
+
+	app.use(express.static('public'));
+	
+	/** creates a mysql connection object to connect to the database
+		Returns: the connection object
+		SECURITY: users should never see this but this information
+					should still be in a seperate file
+	*/
+	function makeConnection(){
+		let con = mysql.createConnection({
+		  host     : 'cs346p2mysqlrockpaper.ccvzhpt8n1yb.us-east-1.rds.amazonaws.com',
+		  user     : 'egamIrorriM',
+		  password : 'KingsHorses',
+		  port     : '3306',
+		  database : 'testdb'
+		});
+		
+		return con;
+		
+	}
+	
+	/** checks if the given username and password are in the database*/
+	function logIn(userName, pass, res) {
+		
+		let con = makeConnection();
+		let json = {};
+
+		con.connect(function(err) {
+			if (err) throw err;
+			
+			let question = "SELECT * FROM 337user WHERE userName='" + userName + "' AND password ='"+ pass +"'";
+			
+			con.query(question, function (err, result, fields) {
+				if (err) throw err;
+				
+				if(result.length <= 0){
+					json["result"] = null;
+					res.send(json);
+				} else {
+					json["result"] = "success";
+					json["clicks"] = result[0]["clicks"];
+					addSession(con, userName, json, res);
+				}
+			});
+		});
+		
+	}
+	
+	/** checks if the session id is valid and returns username and clicks if so
+	*/
+	
+	function checkSession(sessionID, res){
+		
+		let json = {};
+		
+		if( parseInt(sessionID) < 0){
+			json["result"] = null;
+			res.send(json);
+		}
+		let con = makeConnection();
+		
+
+		con.connect(function(err) {
+			if (err) throw err;
+			
+			let question = "SELECT * FROM 337user WHERE session='" + sessionID + "'";
+			
+			con.query(question, function (err, result, fields) {
+				if (err) throw err;
+				
+				if(result.length !=1){
+					json["result"] = null;
+					res.send(json);
+				} else {
+					json["result"] = "success";
+					json["userName"] = result[0]["userName"];
+					json["clicks"] = result[0]["clicks"];
+					res.send(json);
+				}
+			});
+		});
+		
+		
+	}
+	
+	/** adds a new sessionID to the users account
+	*/
+	function addSession(con, userName, json, res) {
+		let session = makeSessionId();
+		json["session"] = session;
+		
+		let update = "UPDATE 337user SET session = '" + session + "' WHERE (userName = '"+ userName +"')";
+			
+		con.query(update, function (err, result, fields) {
+			if (err) throw err;
+			
+			res.send(json);
+		});
+		
+	}
+	
+	/** creates a random sessionID
+	*/
+	function makeSessionId(){
+		let randomNumber = Math.floor(Math.random() * 1000000);
+		return randomNumber;
+	}
+	
+	
+	/** tries to add the given username and password as a new instanceof
+		in the database
+	*/
+	function addUser(userName, pass, res) {
+		
+		let con = makeConnection();
+		
+		con.connect(function(err) {
+			if (err) throw err;
+			
+			let question = "SELECT * FROM 337user WHERE userName='" + userName + "'";
+			
+			con.query(question, function (err, result, fields) {
+				if (err) throw err;
+				
+				if(result.length > 0){
+					res.send("Username already in use.");
+					
+				} else {
+					let insert = "INSERT INTO 337user (userName, password) VALUES ('"+userName +"', '"+pass +"')";
+					
+					con.query(insert, function (err, result) {
+						if (err) throw err;
+						
+						res.send("New account created.");
+						
+					});
+					
+				}
+				
+			});
+		});
+		
+	}
+	
+	
+	/** checks if the given username and password are in the database*/
+	function logIn(userName, pass, res) {
+		
+		let con = makeConnection();
+		let json = {};
+
+		con.connect(function(err) {
+			if (err) throw err;
+			
+			let question = "SELECT * FROM 337user WHERE userName='" + userName + "' AND password ='"+ pass +"'";
+			
+			con.query(question, function (err, result, fields) {
+				if (err) throw err;
+				
+				if(result.length <= 0){
+					json["result"] = null;
+					res.send(json);
+				} else {
+					json["result"] = "success";
+					json["clicks"] = result[0]["clicks"];
+					addSession(con, userName, json, res);
+				}
+			});
+		});
+		
+	}
+	
+	/** ensures there is no problem with the program talking back and forth on the same computer*/
+	app.use(function(req, res, next) {
+		res.header("Access-Control-Allow-Origin", "*");
+		res.header("Access-Control-Allow-Headers",
+		"Origin, X-Requested-With, Content-Type, Accept");
+		next();
+	});
+	
+
+	/** checks that the sessionID is good, and then tries to update the clicks
+	*/
+	function updateClicks(sessionID, clicks, res){
+		
+		let json = {};
+		
+		if( parseInt(sessionID) < 0){
+			json["result"] = null;
+			res.send(json);
+		}
+		
+		let con = makeConnection();
+		
+
+		con.connect(function(err) {
+			if (err) throw err;
+			
+			let question = "SELECT * FROM 337user WHERE session='" + sessionID + "'";
+			
+			con.query(question, function (err, result, fields) {
+				if (err) throw err;
+				
+				if(result.length !=1){
+					json["result"] = null;
+					res.send(json);
+				} else {
+					json["result"] = "success";
+			
+					setClicks(sessionID, clicks, json, con, res);
+			
+				}
+			});
+		});
+		
+	}
+
+	/** sets the number of clicks to the sessionID
+	*/
+	function setClicks(sessionId, clicks, json, con, res){
+		let update = "UPDATE 337user SET clicks = '" + clicks + "' WHERE (session = '"+ sessionId +"')";
+			
+		con.query(update, function (err, result, fields) {
+			if (err) throw err;
+			
+			res.send(json);
+		});
+		
+		
+		
+	}
+
+	
+	/** collects the data sent from rockPaper.js
+		and calles the method required
+	*/
+	app.post('/', jsonParser, function (req, res) {
+		let params = req.body;
+		let request = params.request;
+		let sessionID = params.session;
+		let username = params.userName;
+		let pass = params.pass;
+		let clicks = params.clicks;
+		
+		
+		if(request == "logIn"){
+			
+			logIn(username, pass, res);
+		}
+		
+		if(request == "checkSession"){
+			
+			checkSession(sessionID, res);
+		}
+		
+		if(request == "addUser"){
+			
+			addUser(username, pass, res);
+		}
+		
+		if(request == "updateClicks"){
+			
+			updateClicks(sessionID, clicks, res);
+		}
+		
+	});
+	
+
+	app.listen(3000);
+	
+	
+}) ();
